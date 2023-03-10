c459092330634b38eebd09d5ccde2448e2978dd2
valentina vittoria <valeviit@icloud.com>
2023-03-10 18:48:37 +0100

    create create user Function

--- src/api/controllers/User.ts
+++ src/api/controllers/User.ts
@@ -2,7 +2,24 @@ import { NextFunction,Request,Response } from "express";
 import mongoose from "mongoose";
 import User from "../models/User";
 
-const createUser = (req:Request, res:Response, next: NextFunction) =>{
-    const {  } = req.body;
+const createUser = (req: Request, res: Response, next: NextFunction) => {
+    const { email, books } = req.body;
+    if (email === User.findOne({email})) {
+       return res.status(403).json({message: 'This account already exist!'})
+    } else {
+        const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            email,
+            books
+        })
+        return user
+            .save()
+            .then((user) => res.status(201).json({ user }))
+            .catch((error)=>res.status(500).json({error}))
+    }
+    
+}
+
+const findUser = (req: Request, res: Response, next: NextFunction) => {
     
 }
